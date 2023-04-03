b7988404e28f953c22c7bf95f29c38b906f3819d
stanicarcaleb <78245208+Caleb-Stanicar@users.noreply.github.com>
2023-04-02 22:55:36 -0400

    a

---
+++ website/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body className="App-body">
+          <h1>textofthislength</h1>
+          <h1>textofthatlength</h1>
+          <h1>textforyouandme</h1>
+          <h1>textandtextforalltosee</h1>
+          <h1>textilovetextyouandme</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
